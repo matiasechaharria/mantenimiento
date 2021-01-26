15ea4358750675296c3c2058eff0e7816a26bb06
matias <matiecha49@gmail.com>
2021-01-26 16:09:57 -0300

    subiendo bien notas de comunicacion

--- notascomunicacion/models.py
+++ notascomunicacion/models.py
@@ -11,7 +11,7 @@ class NotaComunicacion(models.Model):
     asunto = models.CharField(max_length=200)
     resumen = models.CharField(max_length=500)
     #archivo = models.ImageField(upload_to='notascomunicacion_file/')
-    #img= models.FileField(null=True,blank=True, default=None, storage=fs  )
+    file= models.FileField(null=True,blank=True, default=None, upload_to='notascomunicacion_file/%Y/%m/%d')
 
 
     #servicio = models.ForeingKey(Servicio,on_delete=models.CASCADE)
